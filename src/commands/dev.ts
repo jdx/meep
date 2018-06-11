6bc463b45f9a1ae50af55b8c7e0e8104af6784fc
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-06-10 17:32:11 -0700

    wip

--- src/commands/dev.ts
+++ src/commands/dev.ts
@@ -10,10 +10,11 @@ const Toml = require('toml')
 const wrapAnsi = require('wrap-ansi')
 
 export interface Meepfile {
-  component?: {
-    [name: string]: {
-      command?: string
-    }
+  component?: { [name: string]: Meepfile.Component }
+}
+export namespace Meepfile {
+  export interface Component {
+    command?: string
   }
 }
 
@@ -26,27 +27,31 @@ export default class Dev extends Command {
     this.parse(Dev)
     const toml: Meepfile = Toml.parse(await fs.readFile('Meepfile.toml', 'utf8'))
     console.dir(toml)
-    const procs = Object.entries(toml.component || {}).map(([name, c]) => {
-      const cwd = path.join(process.cwd(), name)
-      if (!c.command) throw new Error('undefined command')
-      const proc = execa.shell(c.command, {cwd, encoding: 'utf8'})
-
-      for (let stream of ['stdout', 'stderr'] as ('stdout' | 'stderr')[]) {
-        proc[stream]
-        .pipe(new LineTransform())
-        .setEncoding('utf8')
-        .on('data', output => {
-          let header = `${name}: `
-          let lines = wrapAnsi(output, screen[stream] - header.length, {
-            hard: true
-          }).split('\n').map((s: string) => [header, s].join(''))
-          if (!lines.length) return
-          process[stream].write(lines.join('\n') + '\n')
-        })
-      }
-
-      return proc
-    })
+    const procs = Object.entries(toml.component || {})
+    .map(([name, c]) => this.startProc(name, c))
     await Promise.all(procs)
+  }
+
+  private startProc(name: string, c: Meepfile.Component) {
+    const cwd = path.join(process.cwd(), name)
+    if (!c.command) throw new Error('undefined command')
+    const proc = execa.shell(c.command, {cwd, encoding: 'utf8'})
+
+    for (let stream of ['stdout', 'stderr'] as ('stdout' | 'stderr')[]) {
+      proc[stream]
+      .pipe(new LineTransform())
+      .setEncoding('utf8')
+      .on('data', output => {
+        let header = `${name}: `
+        const maxWidth = screen[stream] || 120
+        let lines = wrapAnsi(output, maxWidth - header.length, {
+          hard: true
+        }).split('\n').map((s: string) => [header, s].join(''))
+        if (!lines.length) return
+        process[stream].write(lines.join('\n') + '\n')
+      })
+    }
+
+    return proc
   }
 }
