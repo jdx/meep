d2f4f264b7eaa19e816020d35b998a25519f196c
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-06-12 11:21:56 -0700

    debugging

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -28,6 +28,8 @@ export default class Build extends Command {
 
   async run() {
     const {args} = this.parse(Build)
+    const env = await this.buildEnv(args.envDir)
+    if (env.DEBUG) process.env.DEBUG = env.DEBUG
     const yml = await Meepfile.load(args.buildDir)
     let idx = 0
     for (let [name, c] of Object.entries(yml.components)) {
@@ -41,6 +43,15 @@ export default class Build extends Command {
       await qq.x(`./${buildpackID}/bin/compile`, [buildDir, args.cacheDir, args.envDir])
       await mergeDirs(path.join(buildDir, '.profile.d'), path.join(args.buildDir, '.profile.d'))
       await mergeDirs(path.join(buildDir, '.heroku'), path.join(args.buildDir, '.heroku'))
+      await qq.x(`./${buildpackID}/bin/release`, [buildDir, args.cacheDir, args.envDir])
     }
+  }
+
+  private async buildEnv(dir: string): Promise<{[k: string]: string}> {
+    const env: {[k: string]: string} = {}
+    for (let k of await qq.ls(dir)) {
+      env[k] = await qq.read([dir, k])
+    }
+    return env
   }
 }
